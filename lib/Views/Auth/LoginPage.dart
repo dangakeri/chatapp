5ee5eef4ef16a6b62dcdc46ad4da2865175c151a
dangakeri <dangakeri254@gmail.com>
2023-01-11 09:54:02 +0300

    controllers

--- lib/Views/Auth/LoginPage.dart
+++ lib/Views/Auth/LoginPage.dart
@@ -1,52 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool passwordVisible = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    passwordVisible = true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Chatapp'),
-      ),
+      resizeToAvoidBottomInset: false,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Text('Sign in'),
-              ],
-            ),
+            SizedBox(height: MediaQuery.of(context).size.height * .15),
+            const Text('ChatApp',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: MediaQuery.of(context).size.height * .05),
+            const Center(
+                child: Text(
+              'Sign in to continue',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            )),
+            SizedBox(height: MediaQuery.of(context).size.height * .03),
             Container(
-              child: const TextField(
-                decoration: InputDecoration(
+              width: MediaQuery.of(context).size.width * .8,
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Enter Email',
                 ),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: MediaQuery.of(context).size.height * .02),
             Container(
-              child: const TextField(
-                decoration: InputDecoration(
+              width: MediaQuery.of(context).size.width * .8,
+              child: TextField(
+                controller: passwordController,
+                obscureText: passwordVisible,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Enter Password',
                 ),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: MediaQuery.of(context).size.height * .01),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(passwordVisible
+                        ? Icons.check_box_outline_blank
+                        : Icons.check_box_outlined),
+                    onPressed: () {
+                      setState(
+                        () {
+                          passwordVisible = !passwordVisible;
+                        },
+                      );
+                    },
+                  ),
+                  Text('Show password'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      //forgot password screen
+                    },
+                    child: const Text(
+                      'Forgot Password',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width * .83,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {},
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Does not have an account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                  },
+                )
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * .01),
             SignInButton(
               Buttons.Google,
               onPressed: () {},
             ),
-            SizedBox(height: 10),
-            Center(child: Text('By signing in you are agreeing to our')),
-            Center(child: Text(' Terms and condition'))
+            SizedBox(height: MediaQuery.of(context).size.height * .01),
+            const Center(child: Text('By signing in you are agreeing to our')),
+            const Center(child: Text(' Terms and condition')),
           ],
         ),
       ),
